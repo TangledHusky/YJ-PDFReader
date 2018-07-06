7422ab05892dde88ca5d99bc7f00f7db427be895
李亚军 <liyajun@k12cloud.cn>
2018-07-06 13:32:13 +0800

    init

---
+++ PDFReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDFReader
+//
+//  Created by  atide on 16/6/3.
+//  Copyright © 2016年  atide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDFReader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PDFReader
+//
+//  Created by  atide on 16/6/3.
+//  Copyright © 2016年  atide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
